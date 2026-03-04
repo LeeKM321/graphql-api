7fa94d4a412f59b0db1cbb541e5de70fc68f5fe8
LeeKM321 <stephen4951@gmail.com>
2026-03-04 10:32:05 +0900

    조회를 담당하는 QueryResolver 로직 작성

---
+++ src/main/java/com/codeit/graphql/resolver/QueryResolver.java
@@ -0,0 +1,95 @@
+package com.codeit.graphql.resolver;
+
+import com.codeit.graphql.entity.Author;
+import com.codeit.graphql.entity.Book;
+import com.codeit.graphql.service.AuthorService;
+import com.codeit.graphql.service.BookService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+/**
+ * GraphQL Query Resolver
+ * 데이터 조회를 처리합니다.
+ */
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class QueryResolver {
+
+    private final BookService bookService;
+    private final AuthorService authorService;
+
+    /**
+     * 모든 도서 조회
+     */
+    @QueryMapping
+    public List<Book> books() {
+        log.info("GraphQL Query: books");
+        return bookService.getAllBooksWithAuthor();
+    }
+
+    /**
+     * ID로 도서 조회
+     */
+    @QueryMapping
+    public Book book(@Argument Long id) {
+        log.info("GraphQL Query: book");
+        return bookService.getBookById(id);
+    }
+
+    /**
+     * 제목으로 도서 검색
+     */
+    @QueryMapping
+    public List<Book> searchBooks(@Argument String title) {
+        log.info("GraphQL Query: searchBooks(title={})", title);
+        return bookService.searchBooks(title);
+    }
+
+    /**
+     * 모든 저자 조회
+     */
+    @QueryMapping
+    public List<Author> authors() {
+        log.info("GraphQL Query: authors");
+        return authorService.getAllAuthorsWithBooks();
+    }
+
+    // id로 저자 조회
+    @QueryMapping
+    public Author author(@Argument Long id) {
+        log.info("GraphQL Query: author(id={})", id);
+        return authorService.getAuthorById(id);
+    }
+
+    // 저자별 도서 조회
+    @QueryMapping
+    public List<Book> booksByAuthor(@Argument Long id) {
+        log.info("GraphQL Query: booksByAuthor(authorId={})", id);
+        return bookService.getBooksByAuthor(id);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
